7e776b87c6d5aee8498d0533475102431f34c3b3
Andrea Somaini <thegoldgoat@gmail.com>
2021-02-28 13:27:25 +0100

    Linker CLI commands and sample files

--- src/link.cpp
+++ src/link.cpp
@@ -1,20 +1,40 @@
 #include "Linker/Linker.hpp"
+#include <cstring>
 #include <fstream>
 #include <iostream>
+#include <stdio.h>
 
-int main() {
+#define PRINT_USAGE                                                            \
+  std::cerr << "Usage: " << argv[0]                                            \
+            << " -o <output_path> <input_file1> <input_file2> [...]"           \
+            << std::endl
 
-  std::ifstream inputFile("/tmp/canex");
+int main(int argc, char **argv) {
 
-  if (!inputFile.is_open()) {
-    perror("Couldn't open input object file");
+  if (argc < 4) {
+    PRINT_USAGE;
     return -1;
   }
 
-  Linker linker;
+  if (strcmp(argv[1], "-o")) {
+    PRINT_USAGE;
+    return -1;
+  }
+
   std::vector<std::istream *> inputFiles;
 
-  inputFiles.push_back(&inputFile);
+  for (int i = 3; i < argc; i++) {
+    std::ifstream inputFile(argv[i]);
+
+    if (!inputFile.is_open()) {
+      std::cerr << "Couldn't open input file " << argv[i];
+      perror(argv[i]);
+      return -1;
+    }
+    inputFiles.push_back(new std::ifstream);
+  }
+
+  Linker linker;
 
   linker.link(inputFiles);
 }
